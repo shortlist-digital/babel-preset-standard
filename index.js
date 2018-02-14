c219eae99fab1b4c508ab0667c3111fc39e09cf2
thomasdigby <thomasdigby1@gmail.com>
2018-02-14 14:47:31 +0000

    v0.1.2

--- index.js
+++ index.js
@@ -7,21 +7,20 @@ const config = {
     require.resolve('babel-preset-react')
   ],
   plugins: [
+    require.resolve('babel-plugin-external-helpers'),
     require.resolve('babel-plugin-transform-object-rest-spread'),
-    require.resolve('babel-plugin-syntax-dynamic-import'),
+    require.resolve('babel-plugin-syntax-dynamic-import')
   ]
 }
 
 if (process.env.NODE_ENV === 'production') {
-  config.plugins.push(
-    [
-      require.resolve('babel-plugin-transform-react-remove-prop-types'),
-      {
-        mode: 'wrap',
-        ignoreFilenames: ['node_modules']
-      }
-    ]
-  )
+  config.plugins.push([
+    require.resolve('babel-plugin-transform-react-remove-prop-types'),
+    {
+      mode: 'wrap',
+      ignoreFilenames: ['node_modules']
+    }
+  ])
 }
 
 module.exports = config
